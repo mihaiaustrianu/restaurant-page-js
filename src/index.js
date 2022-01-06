15972fff2681a96916fbc95d2f2769673ab5a9ac
Mihai Austrianu <mihaiaustrianu@gmail.com>
2022-01-06 16:51:08 +0200

    small-webpack-update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('so');
+import './styles.css';
+
+function component(){
+    const headline = document.createElement('div');
+    headline.classList.add('headline');
+    const title = document.createElement('h2');
+    title.innerText = "Mihai's"
+
+    headline.appendChild(title);
+    return headline;
+}
+content = document.querySelector('.content')
+content.appendChild(component());
